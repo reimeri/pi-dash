82ba413bd99573b352ad16a5f24c5e3948cbd24e
reimeri <reimeri@tuta.io>
2026-08-04 19:25:45 +0300

    Enable inspector

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -2,4 +2,12 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   preprocess: vitePreprocess(),
+  vitePlugin: {
+    inspector: {
+      toggleKeyCombo: "alt-x",
+      holdMode: false,
+      showToggleButton: "always",
+      toggleButtonPos: "bottom-right",
+    },
+  },
 };
